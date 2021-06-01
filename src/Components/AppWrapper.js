2779951e234540b60913133b9b84174eefadc0d6
John <souzajohnlee@gmail.com>
2021-06-01 16:32:47 -0700

    use context for app wrapper

--- src/Components/AppWrapper.js
+++ src/Components/AppWrapper.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ThemeContext } from "Contexts/ThemeContext";
 
-export default function AppWrapper({children, theme}) {
-    return (
-        <div className={`app bg-primary-${theme}`}>
-            {children}
-        </div>
-    )
+export default function AppWrapper({ children }) {
+  const { theme } = useContext(ThemeContext);
+  return <div className={`app bg-primary-${theme}`}>{children}</div>;
 }
